438259e33cff80187296601058e3ca9d4123bb23
badhon <108542798+badhon496@users.noreply.github.com>
2022-07-09 18:51:28 +0600

    Create  1050 - DDD.py

---
+++ 1050 - DDD.py
@@ -0,0 +1,28 @@
+user_input = int(input())
+
+if user_input==61:
+    print("Brasilia")
+
+elif user_input==71:
+    print("Salvador")
+
+elif user_input== 11:
+    print("Sao Paulo")
+
+elif user_input== 21:
+    print("Rio de Janeiro")
+
+elif user_input== 32:
+    print("Juiz de Fora")
+
+elif user_input== 19:
+    print("Campinas")
+
+elif user_input== 27:
+    print("Vitoria")
+
+elif user_input== 31:
+    print("Belo Horizonte")
+
+else:
+    print("DDD nao cadastrado")
